75fcfbb4d29e262d01de07c938edfa9bde5d3c18
Tommy <tommy.nguyen.13810@gmail.com>
2021-08-24 12:29:17 -0700

    connected mongo

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost:27017/Options', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!")
+    })
+    .catch(err => {
+        console.log("MONGO CONNECTION FAILED")
+        console.log(err)
+    })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
