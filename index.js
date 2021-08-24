34de29fac504ffc797fc22f3b754e15720333f2e
Joshua Aguilar <jaguilar@proofpoint.com>
2021-08-24 13:00:44 -0700

    adding first routes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const app = express()
 const port = 3000
 
+
+
+
 mongoose.connect('mongodb+srv://admin:somepassword@cluster0.ej23h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!")
@@ -12,9 +15,22 @@ mongoose.connect('mongodb+srv://admin:somepassword@cluster0.ej23h.mongodb.net/my
         console.log(err)
     })
 
+//render if we want.
+
+
+//Routes to have to get stock info. 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+//Routes to have to get stock info. 
+app.get('/getuser', (req, res) => {
+  res.json({ user: 'tommy' })
+})
+//setup client-side
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
