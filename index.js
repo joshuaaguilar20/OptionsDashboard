bbed12cf607ae87fc22578171c7cfad0e8bd13e6
Joshua Aguilar <jaguilar@proofpoint.com>
2021-08-24 12:39:24 -0700

    Files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express()
 const port = 3000
 
-mongoose.connect('mongodb://localhost:27017/Options', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://admin:somepassword@cluster0.ej23h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!")
     })
